b031e102aa67c29bd6b228d0d6c14710647b567c
oki3ed <kokosik3215@gmail.com>
2025-07-07 00:51:00 +0300

    Govno

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCLabelBMFont.hpp>
 #include <Geode/modify/CCScene.hpp>
-#include <fstream>s
+#include <fstream>
 #include <vector>
 #include <filesystem>
 
